e02e3d86ffb20901581dbf00300c96fe82b221f9
OmMankar <138040408+OmMankar@users.noreply.github.com>
2024-06-21 01:38:25 +0530

    Update index.js

--- Backend/index.js
+++ Backend/index.js
@@ -10,7 +10,11 @@ const PORT = process.env.PORT || 5000;
 app.use(express.json());
 
 //unable cors for all routers
-app.use(cors());
+app.use(cors(
+  origin:[""],
+  methods:["POST","PUT","GET"],
+  credentials:true
+));
 
 //import routes for TODO API
 const Routes = require("../Backend/route/websiteRoute");
